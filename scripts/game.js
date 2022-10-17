66471b7d21f02e82cd45d744bced37e2a28087de
Farah M <farah.maria.rahman@gmail.com>
2022-10-17 14:26:09 +0000

    add further functions for starting new game

--- scripts/game.js
+++ scripts/game.js
@@ -7,6 +7,8 @@ let game = {
 
 function newGame() {
     game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
 }
 
 module.exports = { game, newGame };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -30,9 +30,17 @@ describe("game object contains correct keys", () => {
 describe("newGame works correctly", () => {
     beforeAll(() => {
         game.score = 42;
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
         newGame();
     });
     test("should set game score to zero", () => {
         expect(game.score).toEqual(0);
+    });
+    test("should clear the computer sequence array", () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("should clear the player moves array", () => {
+        expect(game.playerMoves.length).toBe(0);
     });
 });
